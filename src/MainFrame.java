9d19f976def3e18d7dfb343326b4b11486684650
Suphawit Buaphan <122143843+SSS-BBB@users.noreply.github.com>
2026-07-19 16:15:22 +0700

    Init

---
+++ src/MainFrame.java
@@ -0,0 +1,46 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private static final String APP_NAME = "AirSafe Battery Service";
+	private static final int APP_WIDTH = 1280;
+	private static final int APP_HEIGHT = 720;
+	
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setTitle(APP_NAME);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(APP_WIDTH, APP_HEIGHT);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+	}
+
+}
